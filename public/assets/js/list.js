3bae34d1395c1779ef43cb592b462818ccec806c
AlexPeralta <alexandra.peralta1@gmail.com>
2018-05-31 20:31:42 -0400

    list updates

--- public/assets/js/list.js
+++ public/assets/js/list.js
@@ -8,6 +8,13 @@ var TestStore1 = {
     state: "FL",
     zip: "30309"
 };
+var TestStore2 = {
+    store_name: "Piggly Wiggly",
+    street: "Georgia Street",
+    city: "Atlanta",
+    state: "GA",
+    zip: "33146"
+};
 var TestProduct1 = {
     product_name: "Bananas",
     price: .65,
@@ -15,33 +22,46 @@ var TestProduct1 = {
 };
 var TestProduct2 = {
     product_name: "Eggs",
-    price: 3.62
+    price: 3.62,
+    StoreId: 12
 };
 
-// $.post("/api/stores", TestStore1).then(function(data){
+// $.post("/api/stores", TestStore2).then(function(data){
 //     console.log(data);
 // });
-// $.post("/api/products", TestProduct1).then(function(data){
+// $.post("/api/products", TestProduct2).then(function(data){
 //     console.log("ADDING PRODUCT?!");
 //     console.log(data);
 // })
+$(document).ready(function() {
+    $('#btnSearch').on('click', function(){
+        console.log("button clicked");
+        var productSearched = $("#productSearch").val();
+        //find the product from the db using search like
+        $.get("/api/products/" + productSearched, function(data){
+            console.log(data);
+            renderResultsList(data);
+        });
+        //add it to the array and display it
+        $(this).blur();
+    });
+})
+    
+    
 
 // choose product click event that saves event to favorites
-$('body').on('click', '#btnSearch', function(){
-    var productSearched = $("#productSearch");
-    //find the product from the db using search like
-    $.get("/api/" + productSearched, function(data){
-        console.log(data);
-        renderList(data);
-    });
-    //add it to the array and display it
-    $(this).blur();
-});
 
-function renderList(data){
+function renderResultsList(data){
+    console.log("renderList working?");
     for (var i = 0; i < data.length; i++) {
-        var div = $("<div>");  
-        div.append(data[i]);        
-        $("#list").append(div);  
+        var productResultDiv = $("<div>");
+        productResultDiv.attr("id", "product-result-" + i);  
+        productResultDiv.append("<h3>" + data[i].product_name + "</h3>");
+        productResultDiv.append("<h3>" + data[i].price + "</h3>");
+        productResultDiv.append("<h3>" + data[i].Store.store_name + "</h3>");
+        productResultDiv.append("<h3>" + data[i].Store.street + "</h3>");
+        productResultDiv.append("<h3>" + data[i].Store.city + ", " + data[i].Store.state + " " + data[i].Store.zip + "</h3>");        
+        // productResultDiv.onclick = addToList();        
+        $("#list").append(productResultDiv);  
       }
 }
